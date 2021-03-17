79b18cd7fb2a280ab2a3bb80c370f20ebbdba2c9
Alex Hong <hongalex@google.com>
2021-03-16 17:47:00 -0700

    hello: changes for v0.1.0

--- hello.go
+++ hello.go
@@ -3,9 +3,13 @@ package hello
 import "rsc.io/quote/v3"
 
 func Hello() string {
-    return quote.HelloV3()
+	return quote.HelloV3()
 }
 
 func Proverb() string {
-    return quote.Concurrency()
+	return quote.Concurrency()
+}
+
+func Glass() string {
+	return quote.GlassV3()
 }
